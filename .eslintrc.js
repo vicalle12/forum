18d26a695ab2d4b6d3bbc1c4e6bf1060bdd80606
victor del valle <victordelvalle@MacBook-Pro-de-victor.local>
2018-07-14 00:59:06 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 1,
+    "indent": ["error", 4],
+    // trailing coma allowed
+    "comma-dangle": ["error", "only-multiline"],
+    "no-restricted-syntax": process.env.NODE_ENV === 'production' ? 2 : 1,
+  }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello">
+        <h1>Welcome to the forum</h1>
+        <div v-for="thread in threads">
+            <h2>{{thread.title}}</h2>
+            <div v-for="postId in thread.posts">
+                <p>{{users[posts[postId]]}}</p>
+                <p>{{posts[postId].text}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            threads: sourceData.threads,
+            posts: sourceData.posts,
+            users: sourceData.users,
+        };
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
